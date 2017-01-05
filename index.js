dbed82cd8f897fc7d1ac8a2f7d7b7f6dbe7728a2
kongnet <kongnet@rui10.com>
2017-01-05 11:34:33 +0800

    增加依赖

--- index.js
+++ index.js
@@ -20,20 +20,20 @@ let dbOpt = function(tbName) {
       return me;
     };
     me.exec = function*(ifTrans) {
-      let sql = me.get();
-      let r;
-      if (ifTrans) {
-        try {
-          sql = `begin;${sql}commit;`;
-          r = yield mysql.query(sql);
-          return r;
-        } catch (e) {
-          yield mysql.query('rollback;');
-          let err = e.toString();
-          /'(.+)'/gm.test(err);
-          let light = RegExp.$1;
-          //NOTICE:不要使用ctrl+alt+F 格式化代码
-          $.log(sql.replace(light, `${$.c.red}${light}${$.c.none}`), `\n${err.replace(/('.+')/gm,`${$.c.yellow}$1${$.c.none}`)}`);
+        let sql = me.get();
+        let r;
+        if (ifTrans) {
+          try {
+            sql = `begin;${sql}commit;`;
+            r = yield mysql.query(sql);
+            return r;
+          } catch (e) {
+            yield mysql.query('rollback;');
+            let err = e.toString();
+            /'(.+)'/gm.test(err);
+            let light = RegExp.$1;
+            //NOTICE:不要使用ctrl+alt+F 格式化代码
+            $.log(sql.replace(light, `${$.c.red}${light}${$.c.none}`), `\n${err.replace(/('.+')/gm,`${$.c.yellow}$1${$.c.none}`)}`);
           $.log(`${$.c.green}Rollback${$.c.none}`);
           return -1;
         }
